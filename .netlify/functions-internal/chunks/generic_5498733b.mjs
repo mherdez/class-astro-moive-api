9dd339f07e2ea8bcb6509b2d14516fdc236889ba
Manuel Herdez <herdez@gmail.com>
2023-11-29 21:05:43 -0600

    SSR error por depurar

--- .netlify/functions-internal/chunks/generic_5498733b.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_df91a6d6.mjs');
-
-export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify()
 });
